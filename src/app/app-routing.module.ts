bcad91bcae5619152931f3a2d2120d116aae309a
Freddy Montes <freddymontes@gmail.com>
2019-04-02 22:06:51 -0600

    Add our first routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { CostaRicaComponent } from './pages/costa-rica/costa-rica.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: '',
+        component: HomeComponent
+    },
+    {
+        path: 'cr',
+        component: CostaRicaComponent
+    }
+];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
